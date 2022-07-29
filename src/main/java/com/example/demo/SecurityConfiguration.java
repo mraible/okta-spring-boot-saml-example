37d62cf3c262554a250301dba8a251b7ed48e349
Matt Raible <matt.raible@okta.com>
2022-07-29 14:05:38 -0600

    Change to Set to eliminate duplicate roles

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.saml2.provider.service.authentication.OpenSaml4AuthenticationProvider;
@@ -23,16 +23,21 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfiguration {
 
     private Converter<OpenSaml4AuthenticationProvider.ResponseToken, Saml2Authentication> groupsConverter() {
-		Converter<ResponseToken, Saml2Authentication> delegate = OpenSaml4AuthenticationProvider.createDefaultResponseAuthenticationConverter();
-		return (responseToken) -> {
-			Saml2Authentication authentication = delegate.convert(responseToken);
-			Saml2AuthenticatedPrincipal principal = (Saml2AuthenticatedPrincipal) authentication.getPrincipal();
-			List<String> groups = principal.getAttribute("groups");
-			List<GrantedAuthority> authorities = new ArrayList<>();
-			authorities.addAll(authentication.getAuthorities());
-			groups.stream().map(SimpleGrantedAuthority::new).forEach(authorities::add);
-			return new Saml2Authentication(principal, authentication.getSaml2Response(), authorities);
-		};
+
+        Converter<ResponseToken, Saml2Authentication> delegate =
+            OpenSaml4AuthenticationProvider.createDefaultResponseAuthenticationConverter();
+
+        return (responseToken) -> {
+            Saml2Authentication authentication = delegate.convert(responseToken);
+            Saml2AuthenticatedPrincipal principal = (Saml2AuthenticatedPrincipal) authentication.getPrincipal();
+            List<String> groups = principal.getAttribute("groups");
+            Set<GrantedAuthority> authorities = new HashSet<>();
+            authorities.addAll(authentication.getAuthorities());
+            if (groups != null) {
+                groups.stream().map(SimpleGrantedAuthority::new).forEach(authorities::add);
+            }
+            return new Saml2Authentication(principal, authentication.getSaml2Response(), authorities);
+        };
     }
 
     @Bean
@@ -42,15 +47,15 @@ public class SecurityConfiguration {
         authenticationProvider.setResponseAuthenticationConverter(groupsConverter());
 
         // @formatter:off
-		http
-			.authorizeHttpRequests(authorize -> authorize
-				.anyRequest().authenticated()
-			)
-			.saml2Login(saml2 -> saml2
-				.authenticationManager(new ProviderManager(authenticationProvider))
-			)
-			.saml2Logout(withDefaults());
-		// @formatter:on
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .saml2Login(saml2 -> saml2
+                .authenticationManager(new ProviderManager(authenticationProvider))
+            )
+            .saml2Logout(withDefaults());
+        // @formatter:on
 
         return http.build();
     }
